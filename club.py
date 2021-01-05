17516442858dbffd7cce1076e526d60882465428
L1STOV <listovspartak99@gmail.com>
2021-01-06 01:12:49 +0200

    New clubs will ne append to registered_clubs list + tests

--- club.py
+++ club.py
@@ -1,8 +1,12 @@
 class Club:
+
+    clubs_registered = []
+
     def __init__(self, name, year, colors):
         self.name = name
         self.year = year
         self.colors = colors
+        Club.clubs_registered.append(self.name)
 
     def get_club_info(self):
         return f'{self.name} was found at {self.year}. They colors - {self.colors}'
@@ -10,4 +14,6 @@ class Club:
 
 if __name__ == '__main__':
     arsenal = Club('Arsenal', 1886, 'Red and white')
-    print(arsenal.get_club_info())
+    chelsea = Club('Chelsea', 1905, 'Blue')
+    liverpool = Club('Liverpool', 1892, 'Red')
+    print(', '.join(Club.clubs_registered))
