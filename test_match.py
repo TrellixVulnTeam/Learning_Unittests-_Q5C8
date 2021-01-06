753bdb95ffbd4d74c9a4e49067ca79012eabfcea
L1STOV <listovspartak99@gmail.com>
2021-01-06 14:13:21 +0200

    Tests for set_up_fans function

--- test_match.py
+++ test_match.py
@@ -3,8 +3,15 @@ import unittest
 
 
 class TestMatch(unittest.TestCase):
-    def test_set_up_fans(self):
-        pass
+    def test_set_up_fans_less_than(self):
+        self.assertEqual(match.set_up_fans(6000), 'For today\'s match will come - 6000 fans. That\'s a small number')
+
+    def test_set_up_fans_more_than(self):
+        self.assertEqual(match.set_up_fans(6500), 'For today\'s match will come - 3500 fans. That\'s a normal number')
+
+    def test_set_up_fans_argument_type(self):
+        with self.assertRaises(ValueError):
+            match.set_up_fans('3444')
 
     def test_start_match(self):
         pass
