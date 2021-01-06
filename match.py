7dcf7ccd55c799af36961de741a9e5cd1418b4ea
L1STOV <listovspartak99@gmail.com>
2021-01-06 14:02:15 +0200

    Match class created

---
+++ match.py
@@ -0,0 +1,23 @@
+import club
+import player
+from prettytable import PrettyTable
+
+
+def set_up_fans():
+    pass
+
+
+def start_match():
+    pass
+
+
+def simulate_match():
+    pass
+
+
+def append_match_result_to_table():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ test_match.py
@@ -0,0 +1,16 @@
+import match
+import unittest
+
+
+class TestMatch(unittest.TestCase):
+    def test_set_up_fans(self):
+        pass
+
+    def test_start_match(self):
+        pass
+
+    def test_simulate_match(self):
+        pass
+
+    def test_append_match_result_to_table(self):
+        pass
