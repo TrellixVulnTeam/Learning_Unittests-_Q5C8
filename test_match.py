7493e705798ec9c8d23b1262ceedb8125471ea1b
L1STOV <listovspartak99@gmail.com>
2021-01-06 14:32:23 +0200

    Tests for simulate_match function

--- test_match.py
+++ test_match.py
@@ -20,8 +20,18 @@ class TestMatch(unittest.TestCase):
         with self.assertRaises(ValueError):
             match.start_match(1213, 5123)
 
-    def test_simulate_match(self):
-        pass
+    def test_simulate_match_home_team_wins(self):
+        self.assertEqual(match.simulate_match(3, 2), 'Home team wins. Match score 3:2')
+
+    def test_simulate_match_away_team_wins(self):
+        self.assertEqual(match.simulate_match(2, 3), 'Away team wins. Match score 2:3')
+
+    def test_simulate_match_draw(self):
+        self.assertEqual(match.simulate_match(1, 1), 'Match ended with draw result 1:1')
+
+    def test_simulate_match_argument_types(self):
+        with self.assertRaises(ValueError):
+            match.simulate_match('2', '3')
 
     def test_append_match_result_to_table(self):
         pass
