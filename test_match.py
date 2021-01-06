374b63786700f7536a05bd897f3033a4748e546d
L1STOV <listovspartak99@gmail.com>
2021-01-06 14:50:18 +0200

    New tests added

--- test_match.py
+++ test_match.py
@@ -7,7 +7,7 @@ class TestMatch(unittest.TestCase):
         self.assertEqual(match.set_up_fans(6000), 'For today\'s match will come - 6000 fans. That\'s a small number')
 
     def test_set_up_fans_more_than(self):
-        self.assertEqual(match.set_up_fans(6500), 'For today\'s match will come - 3500 fans. That\'s a normal number')
+        self.assertEqual(match.set_up_fans(6500), 'For today\'s match will come - 6500 fans. That\'s a normal number')
 
     def test_set_up_fans_argument_type(self):
         with self.assertRaises(ValueError):
@@ -16,9 +16,13 @@ class TestMatch(unittest.TestCase):
     def test_start_match(self):
         self.assertEqual(match.start_match('Arsenal', 'Tottenham'), 'Match between Arsenal and Tottenham starts')
 
-    def test_start_match_argument_types(self):
+    def test_start_match_argument_types_int(self):
         with self.assertRaises(ValueError):
             match.start_match(1213, 5123)
+
+    def test_start_match_argument_float(self):
+        with self.assertRaises(ValueError):
+            match.start_match(1213.4, 5123.2)
 
     def test_simulate_match_home_team_wins(self):
         self.assertEqual(match.simulate_match(3, 2), 'Home team wins. Match score 3:2')
@@ -28,6 +32,18 @@ class TestMatch(unittest.TestCase):
 
     def test_simulate_match_draw(self):
         self.assertEqual(match.simulate_match(1, 1), 'Match ended with draw result 1:1')
+
+    def test_simulate_match_argument_type_str(self):
+        with self.assertRaises(ValueError):
+            match.simulate_match('2', '3')
+
+    def test_simulate_match_argument_type_float(self):
+        with self.assertRaises(ValueError):
+            match.simulate_match(2.5, 3.7)
+
+    def test_simulate_match_argument_type_boolean(self):
+        with self.assertRaises(ValueError):
+            match.simulate_match(True, False)
 
     def test_simulate_match_argument_types(self):
         with self.assertRaises(ValueError):
